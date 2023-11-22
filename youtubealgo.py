5b0e3262beaa8c974b3ac657ae9ca2493c480f51
phillipsnozil <149218847+phillipsnozil@users.noreply.github.com>
2023-11-21 21:13:44 -0500

    Update youtubealgo.py

--- youtubealgo.py
+++ youtubealgo.py
@@ -57,3 +57,44 @@ def search_button_clicked():
         result_text.insert(tk.END, f"{i}. {result['title']} (Video ID: {result['video_id']})\n")
 
     result_text.config(state=tk.DISABLED)
+
+
+window = tk.Tk()
+window.title("FINAL YouTube Search")
+
+window.configure(bg='#2E2E2E')
+
+style = ttk.Style()
+
+style.configure('TButton', foreground='#FFFFFF', background='#444444', font=('calibri', 10, 'bold'), borderwidth='4')
+style.configure('TLabel', foreground='#FFFFFF', background='#2E2E2E', font=('calibri', 12, 'bold'))
+style.configure('TEntry', foreground='#000000', background='#FFFFFF', font=('calibri', 12))
+style.configure('TCombobox', foreground='#000000', background='#FFFFFF', font=('calibri', 12))
+
+
+label = ttk.Label(window, text="YouTube Video Search", style='TLabel')
+label.grid(row=0, column=0, columnspan=4, pady=10)
+
+entry = ttk.Entry(window, width=50, font=('calibri', 12))
+entry.grid(row=1, column=0, padx=10, pady=10, columnspan=3)
+
+order_combobox = ttk.Combobox(window, values=["Date", "Alphabetical"], state="readonly", font=('calibri', 12))
+order_combobox.current(0)
+order_combobox.grid(row=1, column=3, padx=10, pady=10)
+
+search_button = ttk.Button(window, text="Search", command=search_button_clicked, style='TButton')
+search_button.grid(row=1, column=4, padx=10, pady=10)
+
+result_text = scrolledtext.ScrolledText(window, width=80, height=15, wrap=tk.WORD, font=('calibri', 11))
+result_text.grid(row=2, column=0, columnspan=5, pady=10)
+
+# Separator
+separator = ttk.Separator(window, orient='horizontal')
+separator.grid(row=3, column=0, columnspan=5, sticky='ew', pady=10)
+
+# Footer
+footer_label = ttk.Label(window, text="© final project by ivan and phillip", font=('calibri', 8), style='TLabel')
+footer_label.grid(row=4, column=0, columnspan=5)
+
+
+window.mainloop()
